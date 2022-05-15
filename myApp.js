f3832a153b3f3872af5bef09789a13c9a2b4e9d2
marina <marina.ssaito@gmail.com>
2022-05-15 14:00:43 -0300

    feat: return json on GET /json

--- myApp.js
+++ myApp.js
@@ -11,4 +11,10 @@ app.get('/', function(req,res) {
 	res.sendFile(absolutePath);
 });
 
+app.get('/json', function(req,res) {
+	res.json({
+		message: 'Hello json',
+	})
+})
+
 module.exports = app;
