a6e0689e4bc44cef892ea653f6d28b071dea4b56
marina <marina.ssaito@gmail.com>
2022-05-15 13:53:58 -0300

    feat: middleware serving static css from public folder

--- myApp.js
+++ myApp.js
@@ -3,10 +3,12 @@ let app = express();
 
 console.log('Hello World');
 
+const publicPath = __dirname + '/public';
+app.use('/public', express.static(publicPath));
+
 app.get('/', function(req,res) {
 	const absolutePath = __dirname + '/views/index.html';
 	res.sendFile(absolutePath);
-})
-
+});
 
 module.exports = app;
