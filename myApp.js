e30b2fcf08b447e4e1fea5679a578035cc461545
marina <marina.ssaito@gmail.com>
2022-05-15 13:29:21 -0300

    feat: return 'Hello Express' on GET /

--- myApp.js
+++ myApp.js
@@ -3,37 +3,9 @@ let app = express();
 
 console.log('Hello World');
 
+app.get('/', function(req,res) {
+  res.send('Hello Express');
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
