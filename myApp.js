a90cad63eb41b2b8cf657ad4f80e49cd04e48b5b
marina <marina.ssaito@gmail.com>
2022-05-16 07:48:14 -0300

    feat: retun json with current date time on GET /now through middleware

--- myApp.js
+++ myApp.js
@@ -8,23 +8,32 @@ console.log('Hello World');
 const publicPath = __dirname + '/public';
 app.use('/public', express.static(publicPath));
 
-app.use(function (req, res, next) {
+app.use(function (req, _res, next) {
 	console.log(`${req.method} ${req.path} - ${req.ip}`);
 	next();
 });
 
-app.get('/', function(req,res) {
+app.get('/', function(_req, res) {
 	const absolutePath = __dirname + '/views/index.html';
 	res.sendFile(absolutePath);
 });
 
-app.get('/json', function(req,res) {
+app.get('/json', function(_req, res) {
 	const messageStyle = process.env.MESSAGE_STYLE;
 	const message = 'Hello json';
 
 	res.json({
 		message: messageStyle === 'uppercase' ? message.toUpperCase() : message,
-	})
+	});
+});
+
+app.get('/now', function(req, _res, next) {
+	req.time = new Date().toString();
+	next();
+}, function(req, res) {
+	res.json({
+		now: req.time,
+	});
 })
 
 module.exports = app;
