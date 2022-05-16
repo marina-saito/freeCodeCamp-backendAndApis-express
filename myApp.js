44f4719bb4166871568b29d0343389d10e04ab1b
marina <marina.ssaito@gmail.com>
2022-05-16 08:01:15 -0300

    feat: route parameter at GET /:word/echo

--- myApp.js
+++ myApp.js
@@ -34,6 +34,13 @@ app.get('/now', function(req, _res, next) {
 	res.json({
 		time: req.time,
 	});
-})
+});
+
+app.get('/:word/echo', function(req, res) {
+	res.json({
+		echo: req.params.word,
+	});
+});
+
 
 module.exports = app;
