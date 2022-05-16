8a8b76f124d96d807182ce97db0c564d93082e36
marina <marina.ssaito@gmail.com>
2022-05-16 07:37:26 -0300

    feat: add root level middleware with log

--- myApp.js
+++ myApp.js
@@ -8,6 +8,11 @@ console.log('Hello World');
 const publicPath = __dirname + '/public';
 app.use('/public', express.static(publicPath));
 
+app.use(function (req, res, next) {
+	console.log(`${req.method} ${req.path} - ${req.ip}`);
+	next();
+});
+
 app.get('/', function(req,res) {
 	const absolutePath = __dirname + '/views/index.html';
 	res.sendFile(absolutePath);
